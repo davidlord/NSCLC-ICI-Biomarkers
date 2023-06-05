1713cdade319f4ea95f5239ce7a503b9917e45c4
davidlord <david.lord96@gmail.com>
2023-06-05 20:06:47 +0200

    added to data preprocessing

--- Data-Preparation/data_preprocessing_v1.py
+++ Data-Preparation/data_preprocessing_v1.py
@@ -19,19 +19,18 @@ os.chdir('/Users/davidlord/Documents/GitHub/NSCLC-ICI-Biomarkers/Data-Preparatio
 #===============================================================
 # DEFINE PARAMETERS
 #===============================================================
+# DEV: Move some of these to config file
 
 # Relative path to data folders
 data_path = './Data'
 
-# Define dictionaries to store dataframes
-clinical_dfs = {}
-mutation_dfs = {}
-
 # File names: 
 clinical_file_name = 'data_clinical_patient.txt'
-#mutation_file_name = 
+sample_file_name = 'data_clinical_sample.txt'
+mutation_file_name = 'data_mutations.txt'
 
 
+# Can remove this re? 
 # Define regular expression to get study name from path
 study_name_re = r'(?<=Data\/).*'
 
@@ -39,43 +38,51 @@ study_name_re = r'(?<=Data\/).*'
 #===============================================================
 # DEFINE FUNCTIONS
 #===============================================================
+                
 
-# Create a list of study data included in Data folder:
+# Create a list of study data included in Data folder
 def get_data_dirs(directory):
     data_dirs = []
     
     for entry in os.scandir(directory):
         if entry.is_dir():
             data_dirs.append(entry.path)
+    print("Data directories included:")
+    for i in data_dirs:
+        print(i)
             
     return data_dirs
 
 
-
-
-
-# Read clinical data files into dictionary of dataframes: 
-# For each included data folder: 
-for path in data_included:
+# Read file that matches name in path into dictionary of dataframe
+# Loop over directories included
+def read_data(path_list, name, data_dict):
+    for path in path_list:
     # Read clinical data files:
-    clinical_file_path = os.path.join(path, clinical_file_name)
-    if os.path.isfile(clinical_file_path):
-        clinical_df = pd.read_csv(clinical_file_path, header=0, delimiter='\t')
-        clinical_dfs[path] = clinical_df
+        file_path = os.path.join(path, name)
+        if os.path.isfile(file_path):
+            df = pd.read_csv(file_path, header=0, delimiter='\t')
+            data_dict[path] = df
+            print("Successfully read: " + file_path)
+
+
+
+# Add column containing study name to each df
+def add_dataframe_name_column(dataframes_dict):
+    for df_name in dataframes_dict:
+        df = dataframes_dict[df_name]
+        df['study_name'] = re.search(r'(?<=Data\/).*', df_name).group()
+
+
+def remove_nonsense_rows(dataframes_dict):
+    for df_name in dataframes_dict:
+        df = dataframes_dict[df_name]
+        df = df.iloc[4:]
+        dataframes_dict[df_name] = df
     
 
-# Define function that adds column containing study name to each df
-def add_dataframe_name_column(dataframes):
-    for name in dataframes:
-        dataframe = dataframes[name]
-        dataframe['study_name'] = re.search(r'(?<=Data\/).*', name).group()
 
-# Run on each dictionary of dfs:
-add_dataframe_name_column(clinical_dfs)
-
-
-
-# Define function that concatinates dataframes in dict: 
+# Concatinate dataframes in dict into single dataframe: 
 def concatinate_dfs(df_dict):
     dfs = []
     for df in df_dict.values():
@@ -84,8 +91,7 @@ def concatinate_dfs(df_dict):
     
     return concatinated_df
 
-# Run on each dictionary of dfs:
-test_concatinated = concatinate_dfs(clinical_dfs)
+
 
 
 #===============================================================
@@ -95,11 +101,46 @@ test_concatinated = concatinate_dfs(clinical_dfs)
 # Get path to directories included in data folder
 data_included = get_data_dirs(data_path)
 
+# Define dictionaries to store dataframes
+clinical_dfs = {}
+sample_dfs = {}
+mutation_dfs = {}
 
+# READ DATA tables into dictionary of dataframes
+    # Clinical data: 
+read_data(data_included, clinical_file_name, clinical_dfs)
+    # Sample data:
+read_data(data_included, sample_file_name, sample_dfs)
+    # Mutational data: 
+read_data(data_included, mutation_file_name, mutation_dfs)
     
 
+# FILTER nonsense rows from dataframes
+    # Clinical data:
+remove_nonsense_rows(clinical_dfs)
+
+# ADD STUDY NAME as column do dataframes in dictionaries
+    # Clinical data: 
+add_dataframe_name_column(clinical_dfs)
+    # Mutational data: 
+
+# CONCATINATE DATAFRAMES in dicts into single dataframe
+    # Clinical data:
+all_clinical_data = concatinate_dfs(clinical_dfs)
 
 
+
+
+
+
+#===============================================================
+# UNDER DEVELOPMENT
+#===============================================================
+
+# Read mutations table into df
+mutations_df = pd.read_csv('Data/nsclc_mskcc_2018/data_mutations_mskcc.txt', header=0, delimiter='\t')
+
+# read sample tables 
 
 
 
