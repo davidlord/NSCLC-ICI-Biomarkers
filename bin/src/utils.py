8e2199f29348c35a71999010e34583d946608241
AlinaO311 <alina.orozco12@gmail.com>
2024-01-25 17:38:43 +0100

    remove Data-Preparation from dev branch

---
+++ bin/src/utils.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+from pathlib import Path
+from typing import Dict, List, Optional
+
+import git
+import oyaml
+
+
+def read_config(config_path: Path) -> dict:
+    """Extract a configuration dict from a .yaml file.
+
+    Arguments:
+        config_path -- The path to the .yaml config file.
+
+    Returns:
+        A dict containing the configuration.
+    """
+    with open(config_path) as file:
+        config = oyaml.safe_load(file)
+    return config
+
+def check_git_status()-> None:
+    # Check status of git repo.
+    repo = git.Repo(search_parent_directories=True)
+    if repo.is_dirty():
+        print(
+            "\nWARNING!! The git repository is dirty. Commit all your changes before running scripts!\n"
+        )
+
+def save_git_status(path: Path) -> None:
+    """Saves the status (git hash) of the git repository.
+
+    Arguments:
+        path -- The path to the folder where the git info will be saved.
+    """
+    repo = git.Repo(search_parent_directories=True)
+    sha = repo.head.object.hexsha
+
+    # Save git status info.
+    with open(Path(path) / "git_info.txt", "w") as file:
+        file.write(f"Git hash: {sha}")
+
+
+def copy_config(config: dict, path_to_folder: Path, name) -> None:
+    """Saves a copy of the configuration.
+
+    Arguments:
+        config -- The configuration dict to store.
+        path_to_folder -- The path to folder of the intended location.
+        name -- The intended name of the configuration file.
+    """
+    assert (
+        path_to_folder.is_dir()
+    ), f"Failed to save config file. {path_to_folder} is not a folder."
+
+    # Save the configuration to disk.
+    with open(path_to_folder / f"{name}.yml", "w") as f:
+        f.write(oyaml.dump(config))
+
+
+def get_current_datetime() -> str:
+    """Returns the current datetime as a string."""
+    return datetime.now().strftime("%Y%m%d-%H%M%S")
+
+
+def create_directory(dir_path: Path) -> None:
+    """Creates a directory at the given path."""
+    Path(dir_path).mkdir(exist_ok=True, parents=True)
+
+
+def prepare_save_folder(
+    output_folder: Path,
+    name: str,
+    subfolders: List[str],
+    configs: Dict[str, dict],
+) -> Path:
+    """Prepares a save folder.
+
+    Arguments:
+        output_folder -- The location of the folder.
+        name -- The name the folder.
+        subfolders -- Names of any subfolders.
+        configs -- The configuration files to store.
+
+    Returns:
+        The path to the prepared folder.
+    """
+    print("Creating output folders.")
+    output_dir = output_folder / f"{name}_{get_current_datetime()}"
+
+    # Create subfolders.
+    for subfolder in subfolders:
+        create_directory(output_dir / subfolder)
+
+    # Store configs.
+    for config_name in configs.keys():
+        copy_config(configs[config_name], output_dir / "config", config_name)
+
+    # Store git status.
+#    save_git_status(output_dir)
+
+    return output_dir
+
