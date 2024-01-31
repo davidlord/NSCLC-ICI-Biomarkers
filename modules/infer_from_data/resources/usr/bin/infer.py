cbd2b6715cc3e8318359c21bf068e20d6001b3b8
AlinaO311 <alina.orozco12@gmail.com>
2024-01-31 14:33:42 +0100

    changed infer module script to include inputs from nf

--- modules/infer_from_data/resources/usr/bin/infer.py
+++ modules/infer_from_data/resources/usr/bin/infer.py
@@ -1,14 +1,18 @@
+#!/usr/bin/env python3
+
+import os
 import argparse
 from pathlib import Path
 from typing import Optional
 
-from src.trainer import Trainer
+from trainer import Trainer
 
 
 def main(experiment_folder: Path, data_path: Path, output_file: Path) -> None:
     assert output_file.suffix == ".csv", "ERROR: Output file does not have `.csv` extension! Exiting."
-    model_config_path = experiment_folder / "config/model_config.yml"
-    model_path = experiment_folder / "model"
+    model_config_path = experiment_folder 
+    pathlist = os.path.dirname(experiment_folder).split('/')[:-1]
+    model_path = os.path.join(*pathlist, 'model')
     trainer = Trainer(model_config_path, resume_model=model_path, data_path=data_path)
     trainer.predict(output_file)
 
@@ -21,25 +25,22 @@ if __name__ == "__main__":
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
-        "-exp",
         "--experiment_folder",
         type=Path,
-        required=True,
-        help="Path to the experiment folder of the trained model.",
+        help="Path to the experiment folder of the trained model." # output/models/xgboost_model/
     )
     parser.add_argument(
-        "-d",
         "--data_path",
         type=Path,
-        help="Path to the data (.csv file) on which to run prediction.",
+        help="Path to the data (.csv file) on which to run prediction.", # test_data.csv
     )
     parser.add_argument(
-        "-o",
         "--output_file",
         type=Path,
         required=True,
         help="Path to file where to save the output data with predictions. "
         "Must end with `.csv`.",
+        default="${params.infer_outfile}"
     )
     args = vars(parser.parse_args())
 
